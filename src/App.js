92138596d61ddaa206e2c43e9efa093146e5722d
Tim Oehler <timoehler@gmail.com>
2020-01-20 16:00:56 -0600

    update

--- src/App.js
+++ src/App.js
@@ -16,28 +16,22 @@ class App extends Component {
       signInWithGoogle,
     } = this.props;
 
+    console.log("user", user)
+
     if (user) {
       return <div className="App">
+        <h1>Hello, {user.displayName}</h1>
         <List/>
         <button onClick={signOut}>Sign out</button>
-
       </div>
     }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
 
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
+               <p>Please sign in.</p>
+              <button onClick={signInWithGoogle}>Sign in with Google</button>
+        
         </header>
       </div>
     );
